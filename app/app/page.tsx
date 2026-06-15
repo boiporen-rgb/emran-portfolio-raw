262dd011a9866af92174c0047b5a303579b785b8
Raihan chowdhury <boiporen@gmail.com>
2026-06-15 19:32:16 +0600

    update

--- app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Portfolio() {
-  return (
-    <div style={{ backgroundColor: '#0d1117', color: '#c9d1d9', minHeight: '100vh', padding: '40px', fontFamily: 'sans-serif' }}>
-      <h1>Al Emran Bin Habib</h1>
-      <p>Journalism Student | Media Enthusiast</p>
-    </div>
-  )
-}
